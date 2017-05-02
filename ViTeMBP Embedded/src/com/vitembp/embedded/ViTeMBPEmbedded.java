32d9cc0eae2ed9575637d83eb23bf1877e023d21
Kyle Grund <me@kylegrund.com>
2017-05-02 13:42:08 -0700

    Initial project creation and import.

---
+++ ViTeMBP Embedded/src/com/vitembp/embedded/ViTeMBPEmbedded.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vitembp.embedded;
+
+/**
+ * Class containing the main entry point for program.
+ */
+public class ViTeMBPEmbedded {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
