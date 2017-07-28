3fe7745bb2d420486c00e64fd4a4eef875541553
Kyle Grund <me@kylegrund.com>
2017-07-28 13:03:22 -0700

    Includes clean-up.

--- ViTeMBP Embedded/src/com/vitembp/embedded/data/CaptureFactory.java
+++ ViTeMBP Embedded/src/com/vitembp/embedded/data/CaptureFactory.java
@@ -22,8 +22,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 /**
  * A factory class that creates Capture instances.
