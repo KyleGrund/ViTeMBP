7416fd0813243a59ace8f83c7165df63d0578385
me@kylegrund.com <>
2017-08-05 20:15:12 -0700

    Adding overlay definition unit tests.

--- ViTeMBP Services/test/com/vitembp/services/imaging/OverlayDefinitionTest.java
+++ ViTeMBP Services/test/com/vitembp/services/imaging/OverlayDefinitionTest.java
@@ -17,6 +17,7 @@
  */
 package com.vitembp.services.imaging;
 
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -85,5 +86,98 @@ public class OverlayDefinitionTest {
         assertEquals(1, result.getElementDefinitions().get(0).getSensors().size());
         assertEquals(ElementLocation.Right, result.getElementDefinitions().get(0).getSensors().get(0).getLocation());
         assertEquals("Right Brake", result.getElementDefinitions().get(0).getSensors().get(0).getName());
-    }    
+    }
+    
+    /**
+     * Test of getElementDefinitions method, of class OverlayDefinition.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testGetElementDefinitionsFullConfig() throws Exception {
+        System.out.println("getElementDefinitions");
+        String xmlDefinition = "<overlay><type>FourQuadrant</type><elements>" +
+                "<element>" +
+                "<type>BrakeSensor</type>" +
+                "<location>BottomRight</location>" +
+                "<sensorbindings>" +
+                "<sensor>" +
+                "<location>Right</location>" +
+                "<name>Right Brake</name>" +
+                "</sensor>" +
+                "<sensor>" +
+                "<location>Left</location>" +
+                "<name>Left Brake</name>" +
+                "</sensor>" +
+                "</sensorbindings>" +
+                "</element>" +
+                "<element>" +
+                "<type>Shock</type>" +
+                "<location>TopLeft</location>" +
+                "<sensorbindings>" +
+                "<sensor>" +
+                "<location>Center</location>" +
+                "<name>Front Shock</name>" +
+                "</sensor>" +
+                "</sensorbindings>" +
+                "</element>" +
+                "<element>" +
+                "<type>Shock</type>" +
+                "<location>TopRight</location>" +
+                "<sensorbindings>" +
+                "<sensor>" +
+                "<location>Center</location>" +
+                "<name>Rear Shock</name>" +
+                "</sensor>" +
+                "</sensorbindings>" +
+                "</element>" +
+                "<element>" +
+                "<type>ThreeAxisG</type>" +
+                "<location>BottomLeft</location>" +
+                "<sensorbindings>" +
+                "<sensor>" +
+                "<location>Center</location>" +
+                "<name>Frame Accelerometer</name>" +
+                "</sensor>" +
+                "</sensorbindings>" +
+                "</element>" +
+                "</elements></overlay>";
+        OverlayDefinition result = OverlayDefinition.getDefinition(xmlDefinition);
+        assertEquals(OverlayType.FourQuadrant, result.getOverlayType());
+        List<ElementDefinition> defns = result.getElementDefinitions();
+        assertEquals(4, defns.size());
+        assertEquals(1L, defns.stream().filter(ed -> ed.getElementType().equals(ElementType.BrakeSensor)).count());
+        assertEquals(1L, defns.stream().filter(ed -> ed.getElementType().equals(ElementType.ThreeAxisG)).count());
+        assertEquals(2L, defns.stream().filter(ed -> ed.getElementType().equals(ElementType.Shock)).count());
+        ElementDefinition brake = defns.stream().filter(ed -> ed.getElementType().equals(ElementType.BrakeSensor)).findFirst().get();
+        ElementDefinition accel = defns.stream().filter(ed -> ed.getElementType().equals(ElementType.ThreeAxisG)).findFirst().get();
+        ElementDefinition shockLeft = defns.stream()
+                .filter(ed -> ed.getElementType().equals(ElementType.Shock))
+                .filter(ed -> ed.getLocation() == ElementLocation.TopLeft)
+                .findFirst().get();
+        ElementDefinition shockRight = defns.stream()
+                .filter(ed -> ed.getElementType().equals(ElementType.Shock))
+                .filter(ed -> ed.getLocation() == ElementLocation.TopRight)
+                .findFirst().get();
+        assertEquals(brake.getLocation(), ElementLocation.BottomRight);
+        assertEquals(2, brake.getSensors().size());
+        assertEquals(1, brake.getSensors().stream().filter((s) -> s.getLocation() == ElementLocation.Left).count());
+        assertEquals(1, brake.getSensors().stream().filter((s) -> s.getLocation() == ElementLocation.Right).count());
+        assertEquals(1, brake.getSensors().stream().filter((s) -> "Left Brake".equals(s.getName())).count());
+        assertEquals(1, brake.getSensors().stream().filter((s) -> "Right Brake".equals(s.getName())).count());
+        
+        assertEquals(accel.getLocation(), ElementLocation.BottomLeft);
+        assertEquals(1, accel.getSensors().size());
+        assertEquals(1, accel.getSensors().stream().filter((s) -> s.getLocation() == ElementLocation.Center).count());
+        assertEquals(1, accel.getSensors().stream().filter((s) -> "Frame Accelerometer".equals(s.getName())).count());
+        
+        assertEquals(shockLeft.getLocation(), ElementLocation.TopLeft);
+        assertEquals(1, shockLeft.getSensors().size());
+        assertEquals(1, shockLeft.getSensors().stream().filter((s) -> s.getLocation() == ElementLocation.Center).count());
+        assertEquals(1, shockLeft.getSensors().stream().filter((s) -> "Front Shock".equals(s.getName())).count());
+        
+        assertEquals(shockRight.getLocation(), ElementLocation.TopRight);
+        assertEquals(1, shockRight.getSensors().size());
+        assertEquals(1, shockRight.getSensors().stream().filter((s) -> s.getLocation() == ElementLocation.Center).count());
+        assertEquals(1, shockRight.getSensors().stream().filter((s) -> "Rear Shock".equals(s.getName())).count());
+    }
 }
