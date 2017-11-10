af767a62202b5f48a84ce1b87554dd1db0241060
Kyle Grund <me@kylegrund.com>
2017-11-09 23:13:24 -0800

    Re-factored the findSyncFrames function to increase clarity and allow easier examination of the performance of various methods.

--- ViTeMBP Services/src/com/vitembp/services/audio/Processing.java
+++ ViTeMBP Services/src/com/vitembp/services/audio/Processing.java
@@ -23,17 +23,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jtransforms.fft.DoubleFFT_1D;
 
 /**
  * Functions for processing audio.
@@ -62,19 +62,147 @@ public class Processing {
         // get data frame chunks of data
         File fileIn = localTempOutput.toFile();
         
-        // provides vidoe file information, such as frame rate
+        // provides video file information, such as frame rate
         VideoFileInfo videoInfo = new VideoFileInfo(new File(sourceFile));
         
-        // the list of values for each frame
-        List<Double> frameValues = new ArrayList<>();
+        // performs an FFT on the audio of each video frame and gets the value
+        // at the target signal frequency
+        List<Double> frameValues = getFrameFFTValues(fileIn, videoInfo, signalFrequency);
         
-        // the number of frames to process (limit to 30 seconds)
-        int frameLimit = (int)Math.round(videoInfo.getFrameRate() * 30);
-        
-        // the lenght of the audio signal in frames
+        // calculate the length of the audio signal in frames
         double signalTimeInSeconds = 2.0;
         int signalFrameLength = (int)Math.round(videoInfo.getFrameRate() * signalTimeInSeconds);
         
+        // find the sync frames
+        Set<Integer> syncFramesFirst = findSyncFramesByFirstCloseToMax(frameValues);
+        Set<Integer> syncFramesRle = findSyncFramesByRunLength(frameValues, signalFrameLength);
+        Set<Integer> syncFramesAvg = findSyncFramesByAveragingWindow(frameValues, signalFrameLength);
+        
+        // remove temp file
+        localTempOutput.toFile().delete();
+        
+        // return frames
+        return new ArrayList<>(syncFramesFirst);
+    }
+
+    /**
+     * Finds sync frames by finding the average value of a sliding window and
+     * selecting the maximum average value.
+     * @param frameValues The frame FFT values.
+     * @param signalFrameLength The length of the signal to find.
+     * @return A set containing the frame which was found, if any.
+     */
+    private static Set<Integer> findSyncFramesByAveragingWindow(List<Double> frameValues, int signalFrameLength) {
+        // holds the sync frame to return
+        Set<Integer> syncFramesFound = new HashSet<>();
+        
+        // cannot operate if the data length is less than the window
+        if (frameValues.size() < signalFrameLength) {
+            return syncFramesFound;
+        }
+        
+        // stores a sorted map of window average value to start frame
+        TreeMap<Double, Integer> averages = new TreeMap<>(Double::compare);
+        
+        // step through all values
+        for (int startFrame = 0; startFrame < frameValues.size() - signalFrameLength; startFrame++) {
+            // average the window
+            double average = 0.0;
+            for (int offset = 0; offset < signalFrameLength; offset++) {
+                average += frameValues.get(startFrame + offset) / signalFrameLength;
+            }
+            
+            // save the value if it doesn't collide, this prefers earlier frames
+            if (!averages.containsKey(average)) {
+                averages.put(average, startFrame);
+            }
+        }
+        
+        // find the largest average
+        syncFramesFound.add(averages.lastEntry().getValue());
+        
+        return syncFramesFound;
+    }
+    
+    /**
+     * Finds sync frames by finding the maximum value and then the longest run
+     * within a percentage of this value of a reasonable length.
+     * @param frameValues The frame FFT values.
+     * @param signalFrameLength The length of the signal to find.
+     * @return A set containing the frame which was found, if any.
+     */
+    private static Set<Integer> findSyncFramesByRunLength(List<Double> frameValues, int signalFrameLength) {
+        Set<Integer> syncFramesFound = new HashSet<>();
+        
+        // find peak FFT value
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < frameValues.size(); i++) {
+            double val = frameValues.get(i);
+            if (val > max) {
+                max = val;
+            }
+        }
+
+        // find first frame with the next 25% of pulse is within %20 of peak
+        double target = 0.8 * max;
+        
+        // the target minimum run length, serves as a reality check
+        int minRunLength = (int)Math.round(signalFrameLength * 0.25);
+        
+        // the target maximum run length, serves as a reality check
+        int maxRunLength = (int)Math.round(signalFrameLength * 1.25);
+        
+        // stores a map of run length to first frame the length was found at
+        TreeMap<Integer, Integer> values = new TreeMap<>(Integer::compare);
+        
+        // step through all values
+        for (int startFrame = 0; startFrame < frameValues.size(); startFrame++) {
+            // if the value is in the target check the run length
+            if (frameValues.get(startFrame) >= target) {
+                for (int runLength = 1; runLength < frameValues.size(); runLength++) {
+                    if (frameValues.get(startFrame + runLength) < target) {
+                        // record run length if not already found
+                        // this prefers earlier frames of same length
+                        if (!values.containsKey(runLength)) {
+                            values.put(runLength, startFrame);
+                        }
+                        
+                        // no need to check before this value
+                        startFrame = startFrame + runLength + 1;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        // if we found any valid runs
+        if (values.size() > 0) {
+            // find the longest run, we only check the longest to be in range,
+            // if it is not we likely either have no signal or our signal to
+            // noise ratio is too low
+            int largestRun = values.lastEntry().getValue();
+            
+            // if the run was within the sanity check range, record it
+            if (largestRun > minRunLength && largestRun < maxRunLength) {
+                syncFramesFound.add(values.get(largestRun));
+            }
+        }
+        
+        return syncFramesFound;
+    }
+    
+    /**
+     * Gets the FFT data for each frame of the video.
+     * @param fileIn The source video file.
+     * @param videoInfo The VideoFileInfo object for the source video file.
+     * @param signalFrequency The target frequency to find.
+     * @return A list of the FFT data for each frame in the video.
+     */
+    private static List<Double> getFrameFFTValues(File fileIn, VideoFileInfo videoInfo, double signalFrequency) {
+        // the list of values for each frame
+        List<Double> frameValues = new ArrayList<>();
+        // the number of frames to process (limit to 30 seconds)
+        int frameLimit = (int)Math.round(videoInfo.getFrameRate() * 30);
         // somePathName is a pre-existing string whose value was
         // based on a user selection.
         try {
@@ -105,7 +233,7 @@ public class Processing {
             int bytesRead;
 
             // create FFT engine
-            org.jtransforms.fft.DoubleFFT_1D fft = new org.jtransforms.fft.DoubleFFT_1D(samplesPerVideoFrame);
+            DoubleFFT_1D fft = new DoubleFFT_1D(samplesPerVideoFrame);
 
             // holds audio data for fft processing
             double[] fftData = new double[samplesPerVideoFrame*2];
@@ -136,84 +264,9 @@ public class Processing {
                 }
             }
         } catch (IOException | UnsupportedAudioFileException e) {
-          LOGGER.error("Unexpected error performing FFT on video audio.", e);
+            LOGGER.error("Unexpected error performing FFT on video audio.", e);
         }
-        
-        // find the sync frames
-        Set<Integer> syncFrames = findSyncFramesByRunLength(frameValues, signalFrameLength);
-        
-        // remove temp file
-        localTempOutput.toFile().delete();
-        
-        // return frames
-        return new ArrayList<>(syncFrames);
-    }
-
-    /**
-     * Finds sync frames by finding the maximum value and then the longest run
-     * within a percentage of this value of a reasonable length.
-     * @param frameValues The frame FFT values.
-     * @param signalFrameLength The length of the signal to find.
-     * @return A set containing the frame which was found, if any.
-     */
-    private static Set<Integer> findSyncFramesByRunLength(List<Double> frameValues, int signalFrameLength) {
-        Set<Integer> syncFramesFound = new HashSet<>();
-        
-        // find peak FFT value
-        double max = Double.MIN_VALUE;
-        for (int i = 0; i < frameValues.size(); i++) {
-            double val = frameValues.get(i);
-            if (val > max) {
-                max = val;
-            }
-        }
-
-        // find first frame with the next 25% of pulse is within %20 of peak
-        double target = 0.8 * max;
-        
-        // the target minimum run length, serves as a reality check
-        int minRunLength = (int)Math.round(signalFrameLength * 0.25);
-        
-        // the target maximum run length, serves as a reality check
-        int maxRunLength = (int)Math.round(signalFrameLength * 1.25);
-        
-        // stores a map of run length to first frame the length was found at
-        Map<Integer, Integer> values = new HashMap<>();
-        
-        // step through all values
-        for (int startFrame = 0; startFrame < frameValues.size(); startFrame++) {
-            // if the value is in the target check the run length
-            if (frameValues.get(startFrame) >= target) {
-                for (int runLength = 1; runLength < frameValues.size(); runLength++) {
-                    if (frameValues.get(startFrame + runLength) < target) {
-                        // record run length if not already found
-                        // this prefers earlier frames of same length
-                        if (!values.containsKey(runLength)) {
-                            values.put(runLength, startFrame);
-                        }
-                        
-                        // no need to check before this value
-                        startFrame = startFrame + runLength + 1;
-                        break;
-                    }
-                }
-            }
-        }
-        
-        // if we found any valid runs
-        if (values.size() > 0) {
-            // find the longest run, we only check the longest to be in range,
-            // if it is not we likely either have no signal or our signal to
-            // noise ratio is too low
-            int largestRun = values.keySet().stream().sorted(Integer::max).findFirst().get();
-            
-            // if the run was within the sanity check range, record it
-            if (largestRun > minRunLength && largestRun < maxRunLength) {
-                syncFramesFound.add(values.get(largestRun));
-            }
-        }
-        
-        return syncFramesFound;
+        return frameValues;
     }
     
     /**
@@ -280,7 +333,7 @@ public class Processing {
         
         // the last(end) byte which must be processed seperately to handle the
         // sign-bit if 
-        int lastByte = 0;
+        int lastByte;
         
         // single byte (8-bit) audio sampling is stored as an unsigned byte
         if (bytesPerSample == 1) {
